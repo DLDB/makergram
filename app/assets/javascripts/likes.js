24db3afa69ab64f2917b6a3eb758dc646f882c81
DLDB <dpbergquist@gmail.com>
2014-06-03 13:06:34 +0100

    added window location to likes.js

--- app/assets/javascripts/likes.js
+++ app/assets/javascripts/likes.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     event.preventDefault();
     $.post(this.href);
   });
-  var connection = new WebSocketRails('localhost:3000/websocket');
+  var connection = new WebSocketRails(window.location.host + '/websocket');
   channel = connection.subscribe('likes');
   channel.bind('new', function(resp) {
     $('.post[data-id=' + resp.id + '] .like_button').html('♥ ' + resp.new_like_count);
